edc6804720830174663058cef85b3e0fdccd4b01
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2023-12-23 18:21:02 +0000

    Finished front end for home page

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,22 +1,24 @@
 import logo from "../assets/img/My_warsaw_logo.png";
-import { basket } from "../assets/img/svg/svgIndex";
+import { basket } from "../assets/img/svgIndex";
 const NavBar = () => {
   return (
     <>
-      <header>
-        <div className="nav-logo">
-          <img src={logo} alt="Logo of the restaurant" />
-        </div>
-        <nav className="nav-links">
-          <a href="#">Home</a>
-          <a href="#">Menu</a>
-          <a href="#">About</a>
-          <a href="#">Location</a>
-        </nav>
-        <div className="basket-svg">
-          <div className="nav-basket">{basket}</div>
-        </div>
-      </header>
+      <div className="nav-container">
+        <header>
+          <div className="nav-logo">
+            <img src={logo} alt="Logo of the restaurant" />
+          </div>
+          <nav className="nav-links">
+            <a href="#">Home</a>
+            <a href="#">Menu</a>
+            <a href="#">About</a>
+            <a href="#">Location</a>
+          </nav>
+          <div className="basket-svg">
+            <div className="nav-basket">{basket}</div>
+          </div>
+        </header>
+      </div>
     </>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,35 @@
 import NavBar from "../components/navBar";
-
+import homePageBg from "../assets/img/home-page-bg.png";
+import { arrowDown } from "../assets/img/svgIndex";
 const HomePage = () => {
   return (
     <>
-      <section className="section-home" id="home">
-        <div className="home container">
-          <NavBar />
+      <section className="home-section" id="home">
+        <NavBar />
+        <div className="home container grid">
+          <div className="home-content grid ">
+            <div className="home-slogan-content">
+              <div className="home-slogans">
+                <div className="small-slogan">
+                  <h3>welcome to my warsaw</h3>
+                </div>
+                <div className="big-slogans">
+                  <h1>Polish Flavours</h1>
+                  <h1>Streetwise</h1>
+                  <h1>Savours</h1>
+                </div>
+              </div>
+              <div className="home-button">
+                <button>order now</button>
+              </div>
+            </div>
+            <div className="home-img">
+              <img src={homePageBg} alt="The picture of well presented food " />
+            </div>
+            <div className="home-arrow">
+              <a className="arrow-svg">{arrowDown}</a>
+            </div>
+          </div>
         </div>
       </section>
     </>
